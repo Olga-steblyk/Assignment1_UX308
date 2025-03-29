48179c70368d62828df89260db1aeb2a312d4ac8
Olga-Stebko <78696791+Olga-steblyk@users.noreply.github.com>
2025-03-29 02:13:36 +0000

    capitalized Clear for button

--- fab.js
+++ fab.js
@@ -67,6 +67,6 @@ document.querySelector("body").insertAdjacentHTML("beforeend", `
     `);
 
 document.querySelector(`#fab${suffix}`).addEventListener("click", evt=>{
-    const blur = new Blur(`<div id="modal${suffix}"><div><x-chat /></div><button id="clear${suffix}">clear</button></div>`);
+    const blur = new Blur(`<div id="modal${suffix}"><div><x-chat /></div><button id="clear${suffix}">Clear</button></div>`);
     document.querySelector(`#clear${suffix}`).addEventListener("click", () => blur.close());
 });
